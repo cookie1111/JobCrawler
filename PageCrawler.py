bafea16e45aa3c573d4223a4bc4bf808d463eb59
sebastjan <sebastjan.kramar@gmail.com>
2022-10-22 22:13:56 +0200

    grabs career page based on a few keywords

--- PageCrawler.py
+++ PageCrawler.py
@@ -1,29 +1,63 @@
 import langdetect as ld
 import requests
+import queue
+from time import sleep
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
 class PageCrawler:
 
-    def __init__(self, company):
+    def __init__(self, company, search_for = ["career", "we-are-recruiting","we-are-hiring", "job", "positions", "welcomekit", "joinus", "join-us", "recruit"]):
+        self.compare_list = search_for
         self.url = company["url"]
-        self.urls_internal = set()
+        self.urls_internal = set(self.url)
+        self.urls_queue = queue.Queue()
+        self.urls_queue.put(self.url)
+        self.potential_job_pages = set()
 
-    """career, careers, we-are-recruiting, we-are-hiring, job, jobs, open-positions, welcomekit, joinus, join-us, recruit"""
-    def search_for_job(self):
 
-        pass
+    def search_for_job_page(self, url):
+        for w in self.compare_list:
+            if w in url:
+                return True
+        return False
 
     def get_about_us(self, urls):
         pass
 
     def map_website(self):
+        #init the crawl
         external_urls = set()
-        urls, external_urls = self.get_page_urls(self.url, external_urls)
+        if self.search_for_job_page(self.url):
+            self.potential_job_pages.add(self.url)
+        urls, external_urls_new = self.get_page_urls(self.url, external_urls)
+        #check external links for jobs
+        self._check_external_urls(external_urls_new)
+        external_urls = external_urls.union(external_urls_new)
+        while not self.urls_queue.empty():
+            url = self.urls_queue.get()
+            if self.search_for_job_page(url):
+                self.potential_job_pages.add(url)
+            try:
+                urls, external_urls_new = self.get_page_urls(url, external_urls)
+            except requests.exceptions.InvalidSchema:
+                print("This url leads nowhere: ", url)
+            self._check_external_urls(external_urls_new)
+            external_urls = external_urls.union(external_urls_new)
+            sleep(0.3)
+        if len(self.potential_job_pages):
+            print(self.url, ": no job page found!")
+
+        return self.potential_job_pages
+
+
+    #possible alternative
+    def find_career_button(self, ):
         pass
 
     def get_page_urls(self, url, external_urls):
         urls = set()
+        external_urls_new = set()
         domain = urlparse(url).netloc
         soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
@@ -42,12 +76,24 @@ class PageCrawler:
 
             if domain not in href:
                 if href not in external_urls:
-                    external_urls.add(href)
+                    external_urls_new.add(href)
                 continue
 
             self.urls_internal.add(href)
+            #print(href)
+            self.urls_queue.put(href)
             urls.add(href)
-        return urls, external_urls
+        return urls, external_urls_new
+
+    def _check_external_urls(self, external_urls):
+        for url in external_urls:
+            pass
+        pass
+
+    #navigate welcome to the jungle page
+    def _welcome_to_the_jungle(self, url):
+        pass
+
 
     def _check_is_valid_url(self, url):
         parse = urlparse(url)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ page_crawler = PageCrawler({"url" : "https://neuralink.com/"})
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(page_crawler.get_page_urls("https://neuralink.com/",set()))
+    print(page_crawler.map_website())
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
