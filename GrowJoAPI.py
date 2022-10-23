acdfcec0817b033cbcb75bba9e784959ba5f90b2
sebastjan <sebastjan.kramar@gmail.com>
2022-10-23 16:24:42 +0200

    dead end directly with api won't go don't know how to authenticate(request header is no go), switching to logging in and going from there

--- GrowJoAPI.py
+++ GrowJoAPI.py
@@ -1,4 +1,12 @@
 import requests
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import seleniumwire.webdriver as webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
 
 """https://growjo.com/api/companies?order=desc&orderBy=employee_growth&offset=0&rowsPerPage=50&filter={"industry":"AI","country":"Germany",}"""
 
@@ -16,7 +24,35 @@ class GrowJoAPI:
 
 
     def _get_auth_and_authorization(self):
-        return 1,1
+        options = webdriver.ChromeOptions()
+
+        wire_opt = {}
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36")
+        #options.add_argument("--headless")
+
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options,
+                                  seleniumwire_options=wire_opt)
+        url = f'https://growjo.com/api/companies?order=desc&orderBy=employee_growth&offset=0&rowsPerPage=50&filter={{"industry":"AI","country":"Germany"}}'
+        driver.get(url)
+        #alert = WebDriverWait(driver, 5).until(EC.alert_is_present())
+        sleep(3)
+        alert = driver.switch_to.alert
+        alert.send_input(self.usr)
+        alert.send_input(Keys.TAB)
+        alert.send_input(self.pwd)
+        sleep(60)
+        alert.accept()
+        ret_id = None
+        for request in driver.requests:
+            if "auth" in request.headers:
+                auth = request.headers["auth"]
+                authorization = request.headers["authorization"]
+                break
+
+        #driver.close()
+        return auth, authorization
 
     def set_country(self,country):
         self.country = country
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from GrowJoAPI import GrowJoAPI
 from PageCrawler import PageCrawler
 from time import sleep
 
+TEST = 2
 
 with open("usr.txt",'r') as f:
     lines = f.read().splitlines()
@@ -15,43 +16,50 @@ grow_jo = GrowJoAPI(usr, pwd, log = True)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    if TEST == 0:
     # crawler tests
-    print(page_crawler.get_page_urls("https://neuralink.com/",set()))
+        print(page_crawler.get_page_urls("https://neuralink.com/",set()))
     #print(page_crawler.map_website())
 
-    # GrowJo tests
-    # empty
-    grow_jo.set_city("")
-    grow_jo.set_country("")
-    grow_jo.set_field("")
-    grow_jo.set_subfield("")
-    print(": ",grow_jo.get_companies())
 
-    sleep(0.2)
+    if TEST == 1:
+        # GrowJo tests
+        # empty
+        grow_jo.set_city("")
+        grow_jo.set_country("")
+        grow_jo.set_field("")
+        grow_jo.set_subfield("")
+        print(": ",grow_jo.get_companies())
 
-    # incorrect
-    grow_jo.set_city("33")
-    grow_jo.set_country("asfasf")
-    grow_jo.set_field("awd")
-    grow_jo.set_subfield("warfaf")
-    print(": ",grow_jo.get_companies())
+        sleep(0.2)
 
-    sleep(0.2)
+        # incorrect
+        grow_jo.set_city("33")
+        grow_jo.set_country("asfasf")
+        grow_jo.set_field("awd")
+        grow_jo.set_subfield("warfaf")
+        print(": ",grow_jo.get_companies())
 
-    # correct with missing fields
-    grow_jo.set_city("")
-    grow_jo.set_country("Germany")
-    grow_jo.set_field("AI")
-    grow_jo.set_subfield("")
-    print(": ",grow_jo.get_companies())
+        sleep(0.2)
 
-    sleep(0.2)
+        # correct with missing fields
+        grow_jo.set_city("")
+        grow_jo.set_country("Germany")
+        grow_jo.set_field("AI")
+        grow_jo.set_subfield("")
+        print(": ",grow_jo.get_companies())
 
-    # correct with missing fields
-    grow_jo.set_city("")
-    grow_jo.set_country("Germany")
-    grow_jo.set_field("AI")
-    grow_jo.set_subfield("")
-    print(": ",grow_jo.get_companies())
+        sleep(0.2)
+
+        # correct with missing fields
+        grow_jo.set_city("")
+        grow_jo.set_country("Germany")
+        grow_jo.set_field("AI")
+        grow_jo.set_subfield("")
+        print(": ",grow_jo.get_companies())
+
+    if TEST == 2:
+        #grow_jo._get_auth_and_authorization()
+        pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
